0d1f5a842ee12b4ea0a508a0d87837a3bbc5ed69
pedrocab4 <pedrohenriquealvescosta4@gmail>
2020-11-10 20:21:23 -0300

    Criação das classe Endereco e Cliente no pacote Model.

---
+++ src/main/java/br/edu/iftm/os/model/Cliente.java
@@ -0,0 +1,22 @@
+
+package br.edu.iftm.os.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Cliente implements Serializable{
+    
+    private  Integer id;
+    private  String nome_fantasia;
+    private  String sobrenome_razao;
+    private  Date   data_nascimento; 
+    private  Date   Data_data_cadastro;
+    private  String cpf_cnpj;
+    private  String rg_inscricao;
+    private  String email;
+    private  String sexo;
+    
+}
--- src/main/java/br/edu/iftm/os/model/Permissao.java
+++ src/main/java/br/edu/iftm/os/model/Permissao.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter @Setter
 public class Permissao implements Serializable {
-
+    
     private Integer id;
     private String nome;
     private String descricao;
